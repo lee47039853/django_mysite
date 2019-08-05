38d8cacd964023823f4c197397b46dfdf22eeb21
lmg <lee47039853@gmail.com>
2019-08-05 14:29:14 +0900

    Initial commit

---
+++ mysite/mysite/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os, sys
+import logging
+
+from django.core.wsgi import get_wsgi_application
+
+path = os.path.abspath(__file__+'/../..')
+if path not in sys.path:
+    sys.path.append(path)
+
+logging.info("path : " + path);
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
